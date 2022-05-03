181bbfff296189721300d5f012f991ec8c0b863b
Serafeim Papastefanos <spapas@gmail.com>
2022-05-03 13:44:06 +0300

    make it work with windows

--- tasks/management/commands/custom_worker.py
+++ tasks/management/commands/custom_worker.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-import django_rq
+import django_rq, os
 from rq import Queue, Worker
 from rq_win import WindowsWorker
 
@@ -17,5 +17,8 @@ class Command(BaseCommand):
         redis_conn = django_rq.get_connection('default')
         
         q = Queue(connection=redis_conn)
-        worker = WindowsWorker([q], exc_handler=my_handler, connection=redis_conn)
+        if os.name == 'nt':
+            worker = WindowsWorker([q], exc_handler=my_handler, connection=redis_conn)
+        else:
+            worker = Worker([q], exc_handler=my_handler, connection=redis_conn)
         worker.work()
--- tasks/management/commands/custom_worker_low.py
+++ tasks/management/commands/custom_worker_low.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-import django_rq
+import django_rq, os
 from rq import Queue, Worker
 from rq_win import WindowsWorker
 
@@ -16,5 +16,9 @@ class Command(BaseCommand):
         redis_conn = django_rq.get_connection('default')
         
         q = Queue(settings.DJANGO_TEST_RQ_LOW_QUEUE, connection=redis_conn)
-        worker = WindowsWorker([q], exc_handler=my_handler, connection=redis_conn)
+        if os.name == 'nt':
+            worker = WindowsWorker([q], exc_handler=my_handler, connection=redis_conn)
+        else:
+            worker = Worker([q], exc_handler=my_handler, connection=redis_conn)
+            
         worker.work()
